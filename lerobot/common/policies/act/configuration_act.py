d3cb90d45bb912e7d43481c890d1aa32e6ed689c
kuehnrobin <kuehnrobin00@gmail.com>
2025-06-17 12:52:20 +0200

    params: latest change of act params

--- lerobot/common/policies/act/configuration_act.py
+++ lerobot/common/policies/act/configuration_act.py
@@ -192,10 +192,10 @@ class OpenTelevisionACTConfig(ACTConfig):
     # Short-horizon settings
     chunk_size: int = 60
     n_action_steps: int = 1 # Wissen wir nicht #  Todo anpassen
-    n_decoder_layers: int = 7
+    n_decoder_layers: int = 3
     # Temporal ensembling for smoothness
     temporal_ensemble_coeff: float = 0.005
-    optimizer_lr: float = 5e-5
+    #optimizer_lr: float = 5e-5
     # epochs 25000
     # Batch size 45
 
